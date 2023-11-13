2a5c64e72181cd1461d8f9cb382eb8a60c04037c
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2023-11-13 15:20:00 -0800

    added

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: void
+ */
+int _puts(char *str)
+{
+	char *a;
+
+	a = str;
+	while (*str)
+		_putchar(*str);
+	return (str - a);
+}
